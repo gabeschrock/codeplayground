e92f84c630b87073443ce658a69f616691e08a9a
Gabe Schrock <gabeschrock@gmail.com>
2023-12-09 16:26:28 -0500

    Tokenize code complete except for strings.

--- js/tokenize.js
+++ js/tokenize.js
@@ -1,4 +1,38 @@
-{}
+var tokenMap = {
+    "\n": "NEWLINE",
+    ",": "COMMA",
+    "(": "L_PAREN",
+    ")": "R_PAREN",
+    "[": "L_BRACKET",
+    "]": "R_BRACKET",
+    "{": "L_BRACE",
+    "}": "R_BRACE",
+    "+": "PLUS",
+    "-": "MINUS",
+    "*": "MULT",
+    "/": "DIV",
+    "~": "BIT_NOT",
+    "&": "BIT_AND",
+    "|": "BIT_OR",
+    "^": "BIT_XOR",
+    "=": "VAR_SET",
+    "+=": "VAR_ADD",
+    "-=": "VAR_SUB",
+    "*=": "VAR_MULT",
+    "/=": "VAR_DIV",
+    "&=": "VAR_AND",
+    "|=": "VAR_OR",
+    "^=": "VAR_XOR",
+    "==": "EQ",
+    "!=": "NOT_EQ"
+};
+
+class TokenizeError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "TokenizeError";
+    }
+}
 
 function tokenize(str, tokenMap) {
     var longestToken = 0;
@@ -8,19 +42,50 @@ function tokenize(str, tokenMap) {
         }
     }
 
+    function isNum(str) {
+        var prefix = str.slice(0, 1)
+        switch (prefix) {
+            case "0b":
+                return /^0b([01]+\.[01]*|\.?[01]+$)/.test(str);
+
+            case "0x":
+                return /^0x([\da-f]+\.[\da-f]*|\.?[\da-f]+)$/i.test(str);
+        }
+        return /^(\d+\.\d*|\.?\d+)$/.test(str);
+    }
+
+    function isVar(str) {
+        return /^\w+$/.test(str);
+    }
+
     var token;
     var tokens = [];
 
-    var varname = "abcdefghijklmnopqrstuvwxyz";
-    varname += varname.toUpperCase();
-    varname += "0123456789_";
+    
+
+    var index;
+    var quote;
 
     while (str) {
         token = str[0];
         str = str.slice(1)
+        if (token == " ") continue;
+        if (token + str[0] == "//") {
+            str = str.replace(/\/.*/, "");
+            continue;
+        }
 
-        if (varname.indexOf(token) + 1 && token * 1 == NaN) {
-            while (varname.indexOf(str[0] + 1) && str) {
+        if (isVar(token) && !isNum(token)) {
+            while (isVar(str[0]) && str) {
+                token += str[0];
+                str = str.slice(1);
+            }
+            tokens.push("VAR " + token);
+            continue;
+        }
+
+        if (isNum(token)) {
+            while (isNum(token + str[0])) {
                 token += str[0];
                 str = str.slice(1);
             }
@@ -28,7 +93,33 @@ function tokenize(str, tokenMap) {
             continue;
         }
 
-        // temporary code
-        tokens += token;
+        token += str.slice(0, longestToken - 1);
+        str = str.slice(longestToken - 1);
+
+        while (!tokenMap[token] && token.length) {
+            str = token.slice(-1) + str;
+            token = token.slice(0, -1)
+        }
+
+        if (token == "")  throw new TokenizeError("unexpected character: " + str[0]);
+        
+        tokens.push(tokenMap[token]);
     }
+    return tokens;
+}
+
+var code = `
+power = func(base, exp)
+    // raise one number to the power of another
+    local ret = base
+    for i in 0 to exp-1 // repeat exp-1 times
+        ret *= base
+    end
+end
+power(3, 10)`
+
+var tokens = tokenize(code, tokenMap)
+console.log(tokens)
+for (var index in tokens) {
+    console.log(tokens[index])
 }
